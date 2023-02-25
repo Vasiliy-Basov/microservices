f8049c744ef2232312e2d3a45541bd00b4a20211
Vasiliy Basov <vasiliy.basov.82@gmail.com>
2023-02-25 23:47:10 +0300

    final kubernetes-2

---
+++ kubernetes/terraform/backend.tf
@@ -0,0 +1,7 @@
+# Указываем путь где мы будем хранить наш stage file
+terraform {
+  backend "gcs" {
+    bucket = "kub-bucket-bd3cf29fda5e855a" # имя нашего bucket
+    prefix = "kuberbetes"
+  }
+}
